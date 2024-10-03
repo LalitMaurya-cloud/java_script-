8dd5ac4be18c2ab401299dd7c9fadf9c906ab43f
Lalit Maurya <lalit.22330@knit.ac.in>
2024-10-03 19:10:29 +0000

    done

--- starting_js/test.js
+++ starting_js/test.js
@@ -1,1 +1,20 @@
 console.log("Lalit")
+console.log("Hello");
+
+let abcTell = 35
+console.log(typeof abcTell);
+
+roomSing = "Anup"
+console.log(typeof roomSing);
+const piT = 465
+console.log(typeof piT);
+var tellRo
+console.table([piT,roomSing,abcTell,tellRo]);
+
+let meT = null
+console.table([piT,roomSing,abcTell,tellRo,meT]);
+console.log(typeof meT);
+
+
+
+    
